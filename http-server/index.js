c60b998299275e0ad2abbd5d72f6cce1869582fd
root <root@Yaswanth2003.localdomain>
2023-11-30 16:08:43 +0530

    Another project

---
+++ http-server/index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const fs = require("fs");
+const args = require("minimist")(process.argv.slice(2));
+
+let registrationContent = "";
+let projectContent = "";
+
+fs.readFile("registration.html", (err, registration) => {
+  if (err) {
+    throw err;
+  }
+  registrationContent = registration;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+        
+      default:
+        response.write(projectContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(args["port"]);
